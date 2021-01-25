ad3bb988ccdc284c6bde02e6d2db7df207cca97c
Rainbow <bowrainradio@gmail.com>
2021-01-25 14:20:16 +0100

    Main files
    Main files

---
+++ script.js
@@ -0,0 +1,32 @@
+
+ 
+
+$('.click_me').on('mouseover',function() {
+
+        var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+        $('.click_me').css({
+          'color' : randomColor,
+          'font-weight' : '600',
+        });
+        
+
+});
+
+$('.click_me').on('mouseout',function() {
+    $('.click_me').css({
+      'color' : '#fff',
+      'font-weight' : 'normal',
+    });
+
+});
+
+
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 100) {
+        $('.bottomMenu').fadeIn();
+    } else {
+        $('.bottomMenu').fadeOut();
+    }
+
+});
