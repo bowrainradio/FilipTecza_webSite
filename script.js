728b519467153e8f5ef4e03836aa0b30d0920d99
bowrainradio <77972019+bowrainradio@users.noreply.github.com>
2024-03-13 20:33:59 -0400

    IP Location TEST

--- script.js
+++ script.js
@@ -65,4 +65,13 @@ $('#bottomBtn').on('click', function() {
   $('.popup').fadeIn();
 });
 
-
+ /* === IP Location TEST === */
+const getIP = async () => {
+    let ip;
+    await fetch('https://api.ipify.org/?format=json')
+    .then(result => result.json())
+    .then(data => {
+        ip = data.ip;
+        console.log(ip)
+    });
+};
