8e0f77a56ff20b10b5fa2d8336977007964b24d8
bowrainradio <77972019+bowrainradio@users.noreply.github.com>
2024-03-25 23:27:03 -0400

    One more try using ipgeolocation api that is free with ssl cert

--- script.js
+++ script.js
@@ -79,3 +79,21 @@ fetch('https://api.ipify.org?format=json')
   });
 });
 */
+
+fetch('https://api.ipgeolocation.io/ipgeo?apiKey=4a0a0e2ab66c48c1830c30d1eb87441c')
+.then(function(response) {
+  response.json().then(jsonData => {
+    // console.log(jsonData)
+    ip_address = jsonData.ip
+    city = jsonData.city
+    state = jsonData.state_prov
+    country = jsonData.country_code3
+    $(".ipAdd").append(ip_address);
+    $(".city").append(city +" | "+state)
+    $(".country").append(country)
+    console.log("I know your IP Adress: "+ip_address)
+    console.log("And you city: "+city)
+    console.log("******Consider getting VPN, if I can find you, others will too******")
+  });
+});
+
